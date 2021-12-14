04a21f60a3d3061a3993a3e3802c2c973dddc634
lykismekk <lykismekk@outlook.com>
2021-12-14 16:08:38 +0800

    Mono simple test

--- trading-service/src/main/java/simpleTestCollections/Part01Flux.java
+++ trading-service/src/main/java/simpleTestCollections/Part01Flux.java
@@ -17,21 +17,21 @@ public class Part01Flux {
 
 //========================================================================================
 
-    // TODO Return an empty Flux
+    // Return an empty Flux
     Flux<String> emptyFlux() {
         return Flux.empty();
     }
 
 //========================================================================================
 
-    // TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+    // Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
     Flux<String> fooBarFluxFromValues() {
         return Flux.just("foo", "bar");
     }
 
 //========================================================================================
 
-    // TODO Create a Flux from a List that contains 2 values "foo" and "bar"
+    // Create a Flux from a List that contains 2 values "foo" and "bar"
     Flux<String> fooBarFluxFromList() {
         ArrayList<String> a = new ArrayList<>(Arrays.asList("foo", "bar"));
         return Flux.fromIterable(a);
@@ -39,14 +39,14 @@ public class Part01Flux {
 
 //========================================================================================
 
-    // TODO Create a Flux that emits an IllegalStateException
+    // Create a Flux that emits an IllegalStateException
     Flux<String> errorFlux() {
         return Flux.error(new IllegalStateException());
     }
 
 //========================================================================================
 
-    // TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
+    // Create a Flux that emits increasing values from 0 to 9 each 100ms
     Flux<Long> counter() {
         Duration period = Duration.of(100L, ChronoUnit.MILLIS);
         return Flux.interval(period).take(10L).log();
