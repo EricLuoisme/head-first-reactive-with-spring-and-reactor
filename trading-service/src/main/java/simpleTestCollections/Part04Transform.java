4673496e375fd6f93ab8108e2a1f8f2c488a9fd0
lykismekk <lykismekk@outlook.com>
2021-12-15 15:54:53 +0800

    map & flatMap simple test

---
+++ trading-service/src/main/java/simpleTestCollections/Part04Transform.java
@@ -0,0 +1,46 @@
+package simpleTestCollections;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Locale;
+
+/**
+ * Learn how to transform values.
+ *
+ * @author Sebastien Deleuze
+ */
+public class Part04Transform {
+
+//========================================================================================
+
+    // Capitalize the user username, firstname and lastname
+    Mono<User> capitalizeOne(Mono<User> mono) {
+        return mono.map(x -> new User(
+                x.getUsername().toUpperCase(),
+                x.getFirstname().toUpperCase(),
+                x.getLastname().toUpperCase()));
+    }
+
+//========================================================================================
+
+    // Capitalize the users username, firstName and lastName
+    Flux<User> capitalizeMany(Flux<User> flux) {
+        return flux.map(x -> new User(
+                x.getUsername().toUpperCase(),
+                x.getFirstname().toUpperCase(),
+                x.getLastname().toUpperCase()));
+    }
+
+//========================================================================================
+
+    // Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+    Flux<User> asyncCapitalizeMany(Flux<User> flux) {
+        return flux.flatMap(this::asyncCapitalizeUser);
+    }
+
+    Mono<User> asyncCapitalizeUser(User u) {
+        return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
+    }
+
+}
